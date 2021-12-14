cb4d70aae7f8e63c0d88adfdf75c43a46da2faaf
Dylan <dylan77120@live.fr>
2021-12-14 15:04:27 +0100

    formulaire contact ( css )

--- src/Form/ContactType.php
+++ src/Form/ContactType.php
@@ -14,9 +14,20 @@ class ContactType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
+            ->add('email',TextType::class,[
+                'required' => false,
+                'label' => 'Votre email',
+                'attr' => array('placeholder' => 'exemple@exemple.fr'),
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Vous devez inscrire votre email.'
+                    ]),
+                ]
+            ])
             ->add('subject',TextType::class,[
                 'required' => false,
                 'label' => 'Sujet du message',
+                'attr' => array('placeholder' => 'Tapez votre mot de passe...'),
                 'constraints' => [
                     new NotBlank([
                         'message' => 'Vous devez renseigner le sujet de votre message.'
@@ -26,36 +37,10 @@ class ContactType extends AbstractType
             ->add('content',TextareaType::class,[
                 'required' => false,
                 'label' => 'Contenu du message',
+                'attr' => array('cols' => '5', 'rows' => '8', 'placeholder' => '  Tapez votre message...'),
                 'constraints' => [
                     new NotBlank([
                         'message' => 'Vous devez rédiger un contenu.'
-                    ]),
-                ]
-            ])
-            ->add('fullname',TextType::class,[
-                'required' => false,
-                'label' => 'Prénom et nom de famille',
-                'constraints' => [
-                    new NotBlank([
-                        'message' => 'Vous devez inscrire votre nom complet.'
-                    ]),
-                ]
-            ])
-            ->add('email',TextType::class,[
-                'required' => false,
-                'label' => 'Votre email',
-                'constraints' => [
-                    new NotBlank([
-                        'message' => 'Vous devez inscrire votre email.'
-                    ]),
-                ]
-            ])
-            ->add('telephone',TextType::class,[
-                'required' => false,
-                'label' => 'Votre téléphone',
-                'constraints' => [
-                    new NotBlank([
-                        'message' => 'Vous devez inscrire votre téléphone.'
                     ]),
                 ]
             ])
