5bae62f8fea8b6dcdce9a0063b355cc5e3daf076
Dylan <dylan77120@live.fr>
2022-02-08 23:46:09 +0100

    suppression d'element de debug

--- src/Controller/CustomerProductController.php
+++ src/Controller/CustomerProductController.php
@@ -2,14 +2,9 @@
 
 namespace App\Controller;
 
-use App\Search\SearchProduct;
-use App\Form\SearchProductType;
 use App\Repository\ProductRepository;
 use App\Repository\CategoryRepository;
 use App\Repository\TagRepository;
-use Knp\Component\Pager\PaginatorInterface;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
